15f6b6801cc27c229d123e49dd16ff5dc87500e9
小罗 <121859606+nft-maker-one@users.noreply.github.com>
2023-05-09 09:01:37 +0800

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -8,11 +8,17 @@ interface IERC20 {
     function balanceOf(address account) external view returns(uint256);//查询余额函数
     function transfer(address to,uint256 amount) external returns(bool);//从当前地址发起转账函数
     function allowance(address owner, address spender) external view returns(uint256);//授权额度查询函数
-    /**
+     /**
     * @dev授权函数
     * @param spender：授权地址，大家其实可以把授权地址比作银行，加密货币总资产比作你所有的钱，你把钱授权给spender相当于把钱存入银行，spender有权对你的钱进行处置
+    * @param vale：表示授权给地址的额度，打击可以比作你存到银行里面钱的总量
+    * /
     function approve(address spender, uint256 value) external returns(bool);
-    
+    /**
+    * @dev转账函数：不同于前面的transfer，本函数不仅可以对自己钱包下的地址进行转账，同时可以操作对自己授权了的地址进行转账
+    * @param from：转账地址
+    * @param to:收款地址
+    * @param amount：转账金额
     function transferFrom(address from, address to,uint256 amount) external returns(bool);
 }
 
